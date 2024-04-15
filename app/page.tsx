24bec0180125b02fca38fe02f4f1d3ccbe6e6543
Idris.S <kiteshi.ssh@gmail.com>
2024-04-15 15:03:28 +0300

    feat(home): adjust responsive layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 export default async function Home() {
   return (
     <main className="grid place-items-center  h-[calc(100vh_-_64px)]">
-      <div className="grid gap-3 place-items-center text-center w-96">
+      <div className="grid gap-3 px-2 place-items-center text-center max-w-96">
         <h1 className="text-2xl font-bold">
-          Welcome to <Code size="lg">supabase & NextJS</Code> Auth template
+          Welcome to <Code size="lg">supabase & NextJS</Code> Auth&nbsp;template
         </h1>
         <p className="font-thin">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic libero
